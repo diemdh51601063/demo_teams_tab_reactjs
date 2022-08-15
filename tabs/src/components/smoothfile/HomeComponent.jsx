89a97cefec6b6b61967dac97ca63634008c3f7c5
NGUYEN THI KIEU DIEM <n-diem@plott.co.jp>
2022-08-15 17:42:20 +0700

    css title button, icon, menu, table, list item with parent

--- tabs/src/components/smoothfile/HomeComponent.jsx
+++ tabs/src/components/smoothfile/HomeComponent.jsx
@@ -1,10 +1,55 @@
+import axios from "axios";
+import jwtDecode from "jwt-decode";
 import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 
 function HomeComponent() {
+
+    const [listDirectory, setListDirectory] = useState([]);
+
+    function getListDirectory() {
+        let token = localStorage.getItem("token");
+        let loginInfo = jwtDecode(token);
+        let userId = loginInfo.data.user_id;
+        axios.get(`https://asean-dev.smoothfile.jp/smoothfile6/admin/api/project/list/02?user_id=${userId}`, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            }
+        }).then(res => {
+            if (res.status == 200) {
+                console.log('111');
+                setListDirectory(res.data.data);
+            }
+        }).catch(error => console.log(error));
+    }
+    useEffect(() => {
+        getListDirectory();
+    }, [])
+
+
     return (
         <>
             <h1>Home Component</h1>
             <a href="/login">Back</a>
+            <div className="menu-directory">
+                <ul>
+                    {listDirectory.map((item, ind) => {
+                        return (
+                            <li key={"project_" + ind}>{item.project_name}
+                                <ul>
+                                    {item.directoriesList.map((dir, k) => {
+                                        return (
+                                            <li>{dir.directory_name}</li>
+                                        )
+                                    })}
+                                </ul>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </div>
         </>
     )
 
--- tabs/src/components/smoothfile/LoginComponent.jsx
+++ tabs/src/components/smoothfile/LoginComponent.jsx
@@ -3,56 +3,43 @@ import "../../assets/smoothfile/login.css";
 import { useForm } from "react-hook-form";
 import axios from "axios";
 import { useHistory } from "react-router-dom";
+import jwtDecode from "jwt-decode";
+import { useEffect } from "react";
 
 function LoginComponent() {
 
-    // const { login,
-    //     handleSubmit,
-    //     formState: { errors }
-    // } = useForm(
-    //     // {
-    //     //     defaultValues: {
-    //     //         url: useRef("url"),
-    //     //         logincode: useRef("login-code"),
-    //     //         password: useRef("password")
-    //     //     }
-    //     // }
-    // );
-    const history = useHistory();
-    const [inputs, setInputs] = useState({
-        login_type: "normal",
-        ldap_list: "0001"
-    });
+    const [listLdap, setListLdap] = useState();
 
-    const handleChange = (event) => {
-        const name = event.target.name;
-        const value = event.target.value;
-        setInputs(values => ({ ...values, [name]: value }))
+    const { register,
+        handleSubmit,
+        formState: { errors }
+    } = useForm();
+    const history = useHistory();
+    useEffect(() => {
+        getLDAPList();
+    }, []);
+
+    async function getLDAPList() {
+        await axios.get(`https://asean-dev.smoothfile.jp/smoothfile6/admin/api/auth/get-ldap-list/02`).then(res => {
+            if (res.status == 200) {
+                setListLdap(res.data.data)
+            }
+        }).catch(error => console.log(error));
     }
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        // let tmpInputs = {
-        //     login_code: "",
-        //     password: "",
-        //     login_type: "normal",
-        //     ldap_list: "0001"
-        // }
-        // tmpInputs.login_code = inputs.login_code;
-        // tmpInputs.password = inputs.password;
-        // console.log(JSON.stringify(tmpInputs));
-
-        axios.post(inputs.url, JSON.stringify(inputs), {
+    const onSubmit = (data) => {
+        axios.post(data.url, JSON.stringify(data), {
             headers: {
                 'Content-Type': 'application/json'
             }
-        }
-        ).then(res => {
-            // console.log(JSON.stringify(inputs));
-            // console.log(res.status);
-            // console.log(res.data);
-            if(res.status == 200) {
+        }).then(res => {
+            if (res.status == 200) {
+                console.log(res.data.data);
+                const loginInfo = jwtDecode(res.data.data.access_token);
+                localStorage.setItem("token", res.data.data.access_token);
+                console.log(loginInfo);
                 history.push('/home');
+
             }
         }).catch(error => console.log(error));
     }
@@ -60,41 +47,39 @@ function LoginComponent() {
     return (
         <div className="login">
             <h1>Login Smoothfile Component</h1>
-            <form className="style-form" onSubmit={handleSubmit}>
+            <form className="style-form" onSubmit={handleSubmit(onSubmit)}>
                 <h3>Login Smoothfile</h3>
                 <div className="field">
-                    <input type="text" required autoComplete="off" id="url"
-                        name="url"
-                        value={inputs.url || ""}
-                        onChange={handleChange} />
+                    <input type="text" required autoComplete="off" id="url" name="url" {...register("url", { required: true })} />
                     <label htmlFor="url" title="URL *"></label>
                 </div>
                 <div className="field">
-                    <input type="text" required autoComplete="off" id="login_code" name="login_code"
-                        value={inputs.login_code || ""}
-                        onChange={handleChange} />
+                    <input type="text" required autoComplete="off" id="login_code" name="login_code" {...register("login_code", { required: true })} />
                     <label htmlFor="login_code" title="Login Code *"></label>
                 </div>
                 <div className="field">
-                    <input type="password" required autoComplete="off" id="password" name="password"
-                        value={inputs.password || ""}
-                        onChange={handleChange} />
+                    <input type="password" required autoComplete="off" id="password" name="password" {...register("password", { required: true })} />
                     <label htmlFor="password" title="Password *"></label>
                 </div>
                 <div className="item-form">
                     <label>Link</label>
-                    <select id="linkldap" name="linkldap"
-                        value={inputs.linkldap || ""}
-                        onChange={handleChange}>
-                        <option value="normal">No link</option>
-                        <option value="haslink">Has link</option>
+                    <select id="link_ldap" name="link_ldap" {...register("link_ldap")}>
+                        {Array.isArray(listLdap) ?
+                            listLdap.map((item, index) => {
+                                return (
+                                    <option value={index}>{item}</option>
+                                )
+                            }) 
+                            :
+                            <option value="normal">No link</option>
+                        }
                     </select>
-
                 </div>
 
                 <div className="item-form">
                     <button type="submit"> <p>Log In</p></button>
                 </div>
+
                 <div className="item-form content">
                     <p>
                         By granting Smooth File for Microsoft Teams access to Smooth File, you are agreeing to Smooth File <a href="#">Terms of Service</a> and <a href="#">Privacy Policy</a>.
@@ -103,6 +88,7 @@ function LoginComponent() {
                         Copyright © <a href="#" >株式会社プロット | Create Next Communication</a> 2022.
                     </p>
                 </div>
+
             </form>
         </div>
     )
