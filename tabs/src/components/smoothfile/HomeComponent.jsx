530f91f9ffa0bc89d86b2b539ae1565ed45cac74
NGUYEN THI KIEU DIEM <n-diem@plott.co.jp>
2022-08-18 17:05:23 +0700

    show tree list ok

--- tabs/src/components/smoothfile/HomeComponent.jsx
+++ tabs/src/components/smoothfile/HomeComponent.jsx
@@ -4,17 +4,38 @@ import React from "react";
 import { useState } from "react";
 import { useEffect } from "react";
 import "../../assets/smoothfile/home.css";
-import { Tree } from '@fluentui/react-northstar'
-import { TriangleDownIcon, TriangleEndIcon } from '@fluentui/react-icons-northstar'
 import { TreeView } from "devextreme-react";
+import { useCallback } from "react";
 
 
 function HomeComponent() {
 
-    const [listDirectory, setListDirectory] = useState([]);
+    const [listProject, setListProject] = useState([]);
     const [displaySubMenu, setDisplaySubMenu] = useState('sub-menu hide-menu');
+    const [listTree, setListTree] = useState([]);
+    const [currentItem, setCurrentItem] = useState();
+    const [listFile, setListFile] = useState([]);
 
-    function getListDirectory() {
+    const selectItem = (e) => {
+        setCurrentItem(e.itemData);
+        let token = localStorage.getItem("token");
+        let loginInfo = jwtDecode(token);
+        let userId = loginInfo.data.user_id;
+        let directoryInfo = e.itemData;
+        axios.get(`https://asean-dev.smoothfile.jp/smoothfile6/admin/api/file/list/02?user_id=${userId}&directory_id=${directoryInfo.directory_id}&project_id=${directoryInfo.project_id}`, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            }
+        }).then(res => {
+            if (res.status == 200) {
+                console.log(res.data.data);
+                setListFile(res.data.data);
+            }
+        }).catch(error => console.log(error));
+    };
+
+    function getListProject() {
         let token = localStorage.getItem("token");
         let loginInfo = jwtDecode(token);
         let userId = loginInfo.data.user_id;
@@ -25,114 +46,66 @@ function HomeComponent() {
             }
         }).then(res => {
             if (res.status == 200) {
-                setListDirectory(res.data.data);
+                setListProject(res.data.data);
+                loadListProjectTest(res.data.data);
             }
         }).catch(error => console.log(error));
     }
     useEffect(() => {
-        getListDirectory();
+        getListProject();
     }, [])
 
-
-    function showListProject() {
-        return (
-            <>
-                <ul>
-                    {
-                        listDirectory.map((project, index) => {
-                            return (
-                                <>
-                                    <li key={"project_" + index} className="sub-menu" >{project.project_name}
-                                        {showListChildDirectory(project.project_id, project.directoriesList)}
-                                    </li>
-                                </>
-                            )
-                        })
+    function loadListProjectTest(list) {
+        let arr = [];
+        list.forEach((project, id) => {
+            let arrRootDir;
+            let j = 0;
+            if (project.directoriesList.length > 0) {
+                project.directoriesList.forEach((directory, key) => {
+                    if (directory.parent_directory_id === "00000000") {
+                        arrRootDir = directory;
+                        arrRootDir.text = directory.directory_name;
+                        arrRootDir.id = "1_" + (id + 1);
+                        arrRootDir.items = setListChild(arrRootDir.id, directory.directory_id, project.directoriesList, project.project_id);
+                        j++;
                     }
-                </ul>
-            </>
-        )
+                });
+                arr.push(arrRootDir);
+            }
+        })
+        setCurrentItem(arr[0]);
+        setListTree(arr);
     }
 
-
-    function showListChildDirectory(projectID, directoriesList) {
-        let arrRootDir = [];
-        let arrayParent = [];
-        let i = 0;
-
-        if (directoriesList.length > 0) {
-            directoriesList.forEach((child, key) => {
-                if (child.parent_directory_id === "00000000") {
-                    arrRootDir.push(child);
-                }
-                if (child.parent_directory_id !== "00000000" && child.project_id === projectID) {
-                    if (child.parent_directory_id === "00000001") {
-                        arrayParent[i] = child;
-                        arrayParent[i]["child"] = setListChild(child.directory_id, directoriesList, projectID);
-                        i++;
-                    }
-                }
-            });
-        }
-        if (arrayParent.length > 0) {
-            return showList(arrayParent);
-        }
-    }
-
-    function setListChild(directoryID, list, projectID) {
+    function setListChild(id, directoryID, list, projectID) {
         let tmpArr = [];
         let i = 0;
         list.forEach((item, key) => {
             if (item.parent_directory_id === directoryID && item.project_id === projectID) {
                 tmpArr[i] = item;
-                tmpArr[i]["child"] = setListChild(item.directory_id, list, projectID);
+                tmpArr[i].text = item.directory_name;
+                tmpArr[i].id = id + "_" + (i + 1);
+                tmpArr[i]["items"] = setListChild(tmpArr[i].id, item.directory_id, list, projectID);
                 i++;
             }
         })
         return tmpArr;
     }
 
-    function showList(arrayParent) {
-        if (arrayParent.length > 0) {
-            // return (
-            //     <ul>
-            //         {arrayParent.map((item, key) => {
-            //             return (
-            //                 <>
-            //                     <li style={{ paddingLeft: '20px' }} key={'item_' + key} className={displaySubMenu} onClick={showSubMenu} > {item.directory_name}
-            //                         {item.child.length > 0 ? (showList(item.child)) : ""}
-            //                     </li>
-            //                 </>
-            //             )
-            //         })}
-            //     </ul>
-            // )
-            let currentItem = arrayParent[0];
+    function showListProject() {
+        console.log(listTree);
+        if (listTree.length > 0) {
             return (
-                <div className="form">
-                    <TreeView id="simple-treeview"
-                        items={arrayParent}
-                        width={300}
-                        onItemClick={arrayParent[0]} />
-                    {currentItem.price
-                        && <div id="product-details">
-                            {/* <img src={currentItem.image} /> */}
-                            <div className="name">{currentItem.text}</div>
-                            <div className="price">{`$${currentItem.price}`}</div>
-                        </div>
-                    }
-                </div>
-            );
+                <>
+                    <div className="form">
+                        <TreeView id="simple-treeview"
+                            items={listTree}
+                            onItemClick={selectItem} />
+                    </div>
+                </>
+            )
         }
     }
-
-    // function showSubMenu(e) {
-    //     if (displaySubMenu === "sub-menu") {
-    //         setDisplaySubMenu("sub-menu hide-menu")
-    //     } else {
-    //         setDisplaySubMenu("sub-menu");
-    //     }
-    // }
 
     return (
         <>
@@ -143,7 +116,17 @@ function HomeComponent() {
                     {showListProject()}
                 </div>
                 <div className="list-file">
-                    <h1>jjjkjkkj</h1>
+                    {listFile.length > 0 ?
+                        listFile.map((file, ind) => {
+                            return (
+                                <>
+                                    <div>{file.file_name}</div>
+                                </>
+                            )
+                        })
+                        :
+                        ""
+                    }
                 </div>
             </div>
 
